5f84cccb006a6132a29c5efe5a97c4a70d76a395
Ashim Sharma <ashimsharma337@gmail.com>
2023-09-14 10:03:03 -0400

    Loops in java

---
+++ Learning01/src/Loops.java
@@ -0,0 +1,28 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		// If we have to repeat task on same instance 
+		// we have for loop, while loop, do while loop,
+		// and enhanced for loop(for-each) used with arrays and collection
+		
+		// syntax
+//		int n = 10;
+//		for(int i = 0; i < n; i++) {
+//			//Initialization, comparison or condition or logic, iteration(increment or decrement)
+//		}
+//		
+//		boolean x = true;
+//		
+//		while(x) {  // only condition mostly used to fetch data form database as long as condition meets
+//			
+//		}
+		
+		int ab = 0;
+		do {
+			System.out.println("I ran!!");
+		}
+		while(ab<0);
+	}
+}
